51dbdf8f262d5b978103ea4aa3a70100ccaeb287
PERIFERIAGROUP\carlosquimbayo <carlosquimbayo@cbit-online.com>
2018-11-01 17:00:51 -0500

    buils v3

--- jenkins/build.groovy
+++ jenkins/build.groovy
@@ -1,3 +1,34 @@
 node {
-  echo ' El oleoducto comenzó '
+  def branchVersion = ""
+
+  stage ('Checkout') {
+    // checkout repository
+    checkout scm
+
+    // checkout input branch 
+    sh "git checkout ${caller.env.BRANCH_NAME}"
+  }
+
+  stage ('Determine Branch Version') {
+    // add maven to path
+    env.PATH = "${tool 'M3'}/bin:${env.PATH}"
+
+    // determine version in pom.xml
+    def pomVersion = sh(script: 'mvn -q -Dexec.executable=\'echo\' -Dexec.args=\'${project.version}\' --non-recursive exec:exec', returnStdout: true).trim()
+
+    // compute proper branch SNAPSHOT version
+    pomVersion = pomVersion.replaceAll(/-SNAPSHOT/, "") 
+    branchVersion = env.BRANCH_NAME
+    branchVersion = branchVersion.replaceAll(/origin\//, "") 
+    branchVersion = branchVersion.replaceAll(/\W/, "-")
+    branchVersion = "${pomVersion}-${branchVersion}-SNAPSHOT"
+
+    // set branch SNAPSHOT version in pom.xml
+    sh "mvn versions:set -DnewVersion=${branchVersion}"
+  }
+
+  stage ('Java Build') {
+    // build .war package
+    sh 'mvn clean package -U'
+  }
 }
