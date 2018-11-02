2993d9b8f5a85a5c2a967f9236122489566df411
PERIFERIAGROUP\carlosquimbayo <carlosquimbayo@cbit-online.com>
2018-11-02 14:00:54 -0500

    no message

--- jenkins/build.groovy
+++ jenkins/build.groovy
@@ -16,4 +16,17 @@ node {
       // Run the maven build 
       bat "${mvnHome}/bin/mvn clean install" 
       
+   stage 'Analysis'
+     def mvnHome = tool 'mvn-default'
+
+     sh "${mvnHome}/bin/mvn -batch-mode -V -U -e checkstyle:checkstyle findbugs:findbugs"
+
+     def checkstyle = scanForIssues tool: [$class: 'CheckStyle'], pattern: '**/target/checkstyle-result.xml'
+     publishIssues issues:[checkstyle]
+
+     def findbugs = scanForIssues tool: [$class: 'FindBugs'], pattern: '**/target/findbugsXml.xml'
+     publishIssues issues:[findbugs]
+
+     def maven = scanForIssues tool: [$class: 'MavenConsole']
+     publishIssues issues:[maven]
 }
