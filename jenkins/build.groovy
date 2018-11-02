86f17e4b2b194fd83a6eea59a20857e4bacbb408
PERIFERIAGROUP\carlosquimbayo <carlosquimbayo@cbit-online.com>
2018-11-02 13:49:21 -0500

    no message

--- jenkins/build.groovy
+++ jenkins/build.groovy
@@ -1,22 +1,19 @@
 node {
- 
    // Mark the code checkout 'stage'.... 
-
    stage ('Checkout') {
       // Get some code from a GitHub repository 
-      git url: 'https://github.com/flezon/RedHat.git'
-      // Get the maven tool. // ** NOTE: This 'M3' maven tool must be configured
+      git url: 'https://g.com/flezon/RedHat.git'
+      // Get the maven tool. // ** NOithubTE: This 'M3' maven tool must be configured
       // ** in the global configuration. 
       def mvnHome = tool 'M3'
 
       env.JAVA_HOME="${tool 'jdk'}"
       env.PATH="${env.JAVA_HOME}/bin:${env.PATH}"
-      bat 'java -version'
-      // Mark the code build 'stage'.... 
+      bat 'java -version'      
    }
-   stage ('Build'){ 
+   // Mark the code build 'stage'.... 
+   stage 'Build' 
       // Run the maven build 
       bat "${mvnHome}/bin/mvn clean install" 
-   }
-   
+      
 }
